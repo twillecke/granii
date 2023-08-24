9782144f840d3c288a88eaa26af7be9c150c2554
Thiago Willecke <thiagogwillecke@gmail.com>
2023-08-24 15:04:54 -0300

    fix css

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -4,7 +4,7 @@ const MenuItem = ({ title, description, price_big, price_small }) => (
             <h3 className="text-rose-900 pb-1 font-medium">{title}</h3>
             <p className="font-medium text-sm text-rose-400">{description}</p>
         </div>
-        <div className="text-rose-900 flex items-center justify-center text-xl font-medium">
+        <div className="text-rose-900 flex items-top justify-center text-xl font-medium">
             <span className="mx-4">{price_small}</span>
             <span>{price_big}</span>
         </div>
