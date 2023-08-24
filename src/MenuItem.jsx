71a991701f656e73b1e9053aef1998e982c91c2d
Thiago Willecke <thiagogwillecke@gmail.com>
2023-08-24 14:53:46 -0300

    Align menu items; minor style fixes

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,14 +1,14 @@
 const MenuItem = ({ title, description, price_big, price_small }) => (
-    <div className="mb-6 flex justify-between">
-        <div>
-            <h3 className="text-rose-900 pb-1 font-medium">{title}</h3>
-            <p className="font-medium text-sm text-rose-400">{description}</p>
-        </div>
-        <div className="text-rose-900 flex items-center justify-center text-xl font-medium">
-            <span className="mx-4">{price_small}</span>
-            <span>{price_big}</span>
-        </div>
-    </div>
+	<div className="mb-6">
+		<div className="mb-0 flex justify-between items-center">
+			<h3 className="text-rose-900 pb-1 text-xl font-medium">{title}</h3>
+			<div className="text-rose-900 flex items-center justify-center text-xl font-medium">
+				<span className="mx-4">{price_small}</span>
+				<span>{price_big}</span>
+			</div>
+		</div>
+		<p className="font-medium pl-1 text-sm text-rose-400">{description}</p>
+	</div>
 );
 
 export default MenuItem;
