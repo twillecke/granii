71c09af6b6b88663a852dc3bca6d92afd6ab84ac
Thiago Willecke <thiagogwillecke@gmail.com>
2023-08-24 14:41:28 -0300

    set new default sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
